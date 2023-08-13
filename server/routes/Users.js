7ed1bfb9b3cbfe07a26568ae3a8fe7a904a4cd82
aditya-bhardwaj01 <aditya.bhardwaj2020@vitstudent.ac.in>
2023-08-13 23:59:30 +0530

    OTP sending to phone enabled some bugs need to be fixed

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql2");
 const bcrypt = require("bcrypt");
-
 const { sign } = require('jsonwebtoken');
 
 const db = mysql.createConnection({
@@ -32,6 +31,29 @@ const checkAvailability = (username, phone, email, res) => {
   })
 }
 
+const sendOTP = (phone, res) => {
+  const otpGen = require("otp-generator");
+  let otp = otpGen.generate(6, { digits: true, upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false });
+
+  var sid = "AC939553fd0e6d48cb2ef698d196f6ee22";
+  var auth_token = "15a3149b0acc44c86220b9dfaa9fd360";
+
+  var twilio = require("twilio")(sid, auth_token);
+
+  twilio.messages
+    .create({
+      from: "+12517583940",
+      to: "+91"+phone,
+      body: `The OTP for the verification of you recipe sharing application is ${otp}`
+    })
+    .then((result) => {
+      res.status(200).json({success: "OTP has been sent!"});
+    })
+    .catch((err) => {
+      res.json({error: "There was an error sending the OTP!"}) 
+    })
+}
+
 router.post("/register/availability", (req, res) => {
   const username = req.body.username;
   const phone = req.body.phone;
@@ -40,9 +62,9 @@ router.post("/register/availability", (req, res) => {
   const execute = async () => {
     checkAvailability(username, phone, email, res)
       .then((result) => {
-        // console.log(result)
-        // sendOTP();
-        res.status(200).json({ success: "OTP sent to you phone and email!" });
+        console.log(phone)
+        sendOTP(phone, res);
+        // res.status(200).json({ success: "OTP sent to you phone and email!" });
       })
       .catch((err) => {
         // console.log(err)
