35f6273c78d2454882956fbd66db083acd4e6938
aditya-bhardwaj01 <aditya.bhardwaj2020@vitstudent.ac.in>
2023-08-14 21:15:52 +0530

    OTP sending to phone ans email enabled

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const mysql = require("mysql2");
+const otpGen = require("otp-generator");
 const bcrypt = require("bcrypt");
 const { sign } = require('jsonwebtoken');
+const { EMAIL, PASSWORD, SID, AUTH_TOKEN } = require("./env");
 
 const db = mysql.createConnection({
   user: "root",
@@ -11,10 +13,12 @@ const db = mysql.createConnection({
   database: "recipe_sharing"
 });
 
+let otpPhone = 0, otpEmail = 0;
+
 const checkAvailability = (username, phone, email, res) => {
   // console.log(username)
   return new Promise((resolve, reject) => {
-    db.query("select * from users where username=? or phone=? or email=?", [username, phone, email],
+    db.query("select * from users where username=? or email=?", [username, email],
       (err, result) => {
         if (err) {
           res.json({ error: "Check your connection or try after sometime." })
@@ -31,27 +35,59 @@ const checkAvailability = (username, phone, email, res) => {
   })
 }
 
-const sendOTP = (phone, res) => {
-  const otpGen = require("otp-generator");
-  let otp = otpGen.generate(6, { digits: true, upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false });
+const sendOTPtoEmail = async (email, res) => {
+  // ------------- FOR EMAIL ------------ //
+  otpEmail = otpGen.generate(6, { digits: true, upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false }); 
+  var nodemailer = require("nodemailer");
 
-  var sid = "AC939553fd0e6d48cb2ef698d196f6ee22";
-  var auth_token = "15a3149b0acc44c86220b9dfaa9fd360";
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: EMAIL,
+      pass: PASSWORD
+    }
+  });
+
+  var mailOptions = {
+    from: EMAIL,
+    to: email,
+    subject: "OTP for creating account on Recipe Sharing",
+    html: 
+    `<!DOCTYPE html><html lang="en"><head><title>User response</title></head><body>OTP for Recipe Sharing application is ${otpEmail}</body></html>`
+  };
+  
+  await transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      res.json({error: "There was an error sharing the OTP"});
+    } else {
+      res.json({success: "OTP sent to you phone and email"});
+    }
+  });
+}
+
+const sendOTPtoPhone = async (phone, email, res) => {
+  // ------------- FOR PHONE ------------ //
+  otpPhone = otpGen.generate(6, { digits: true, upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false });  
+
+  var sid = SID;
+  var auth_token = AUTH_TOKEN;
 
   var twilio = require("twilio")(sid, auth_token);
+  // ------------- FOR PHONE ------------ //
 
   twilio.messages
     .create({
       from: "+12517583940",
       to: "+91"+phone,
-      body: `The OTP for the verification of you recipe sharing application is ${otp}`
+      body: `The OTP for the verification of you recipe sharing application is ${otpPhone}`
     })
-    .then((result) => {
-      res.status(200).json({success: "OTP has been sent!"});
+    .then(() => {
+      sendOTPtoEmail(email, res);
     })
-    .catch((err) => {
-      res.json({error: "There was an error sending the OTP!"}) 
+    .catch(() => {
+      res.status(500).json({error: "There was an error sending the OTP!"}) 
     })
+
 }
 
 router.post("/register/availability", (req, res) => {
@@ -61,13 +97,10 @@ router.post("/register/availability", (req, res) => {
 
   const execute = async () => {
     checkAvailability(username, phone, email, res)
-      .then((result) => {
-        console.log(phone)
-        sendOTP(phone, res);
-        // res.status(200).json({ success: "OTP sent to you phone and email!" });
+      .then(() => {
+        sendOTPtoPhone(phone, email, res);
       })
-      .catch((err) => {
-        // console.log(err)
+      .catch(() => {
         res.status(200).json({ error: "An account with matching credentials already exist!" });
       })
   }
