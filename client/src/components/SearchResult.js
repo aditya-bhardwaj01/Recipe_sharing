748a79cf29554746d790db8de169bb48be8b45d0
aditya-bhardwaj01 <aditya.bhardwaj2020@vitstudent.ac.in>
2023-08-31 15:38:16 +0530

    Frotend for search result

--- client/src/components/SearchResult.js
+++ client/src/components/SearchResult.js
@@ -52,7 +52,54 @@ export default function SearchResult() {
                                 <div className="searchresult-display">
                                     {items.map((item, index) => (
                                         <div key={index} className="searchresult-single">
-                                            {item.title}
+                                            <div className="single-result-section section-1 row">
+                                                <div className="section-11 col-sm-4">
+                                                    <img src={item.photo} alt="" />
+                                                    <button type="button" class="btn btn-success btn-lg btn-block">View</button>
+                                                </div>
+                                                <div className="section-12 col-sm-8">
+                                                    <h2>{item.title}</h2>
+                                                    <p>{item.name}</p>
+                                                    <p>{item.cooking_method}</p>
+                                                    <p>{item.cuisines}</p>
+                                                    <p>{item.courses}</p>
+                                                    <p>{item.payment == null ? 
+                                                        <span className="badge bg-success">FREE</span> :
+                                                        <span className="badge bg-primary">&#8377;{item.payment}</span>}
+                                                    </p>
+                                                </div>
+                                            </div>
+
+                                            <div className="single-result-section section-2 row">
+                                                <div className="section-21 col-3">
+                                                    {item.difficulty}
+                                                </div>
+                                                <div className="section-22 col-9">
+                                                    <div className="row">
+                                                        <span className='col-3' style={{textAlign: 'center'}}>
+                                                            {item.prep_time}
+                                                        </span>
+                                                        <span className='col-3' style={{textAlign: 'center'}}>
+                                                            {item.cook_time}
+                                                        </span>
+                                                        <span className='col-3' style={{textAlign: 'center'}}>
+                                                            {item.rest_time}
+                                                        </span>
+                                                        <span className='col-3' style={{textAlign: 'center'}}>
+                                                            {item.total_time
+                                                        }</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div className="single-result-section section-3 row">
+                                                <div className="section-31 col-3" style={{textAlign: 'center'}}>
+                                                    {item.calories}
+                                                </div>
+                                                <div className="section-32 col-9" style={{textAlign: 'center'}}>
+                                                    {item.best_season}
+                                                </div>
+                                            </div>
                                         </div>
                                     ))}
                                 </div>
